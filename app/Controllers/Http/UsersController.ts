0bf5d4be318cc3c3b0936f4e3cf71a1992307aa5
gutkedu <eduardo.pedogutkoski@gmail.com>
2022-03-28 14:46:44 -0300

    ✨ feat/user_controller

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -1,19 +1,46 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import User from 'App/Models/User';
 
 export default class UsersController {
-  public async index({ }: HttpContextContract) {
-    return 'all users'
+  public async index({ response }: HttpContextContract) {
+    const user = await User.all()
+    return response.status(200).send(user)
   }
-  public async store({ }: HttpContextContract) {
-    return 'create users'
+
+  public async store({ request, response }: HttpContextContract) {
+    const { name, email, password } = request.body();
+    const searchPayload = { email: email }
+    const savePayload = {
+      name: name,
+      password: password,
+    }
+    await User.firstOrCreate(searchPayload, savePayload)
+    return response.status(201);
   }
-  public async show({ }: HttpContextContract) {
-    return 'get user by id'
+
+  public async show({ request, response }: HttpContextContract) {
+    const { id } = request.params();
+    const user = await User.findOrFail(id)
+    return response.status(200).send(user)
   }
-  public async update({ }: HttpContextContract) {
-    return 'change user settings'
+
+  public async update({ request, response }: HttpContextContract) {
+    const { id } = request.params();
+    const { name, email } = request.body();
+    const user = await User.findOrFail(id);
+    await user
+      .merge({
+        name: name,
+        email: email,
+      })
+      .save()
+    return response.status(200).send(user)
   }
-  public async destroy({ }: HttpContextContract) {
-    return 'delete user'
+
+  public async destroy({ request, response }: HttpContextContract) {
+    const { id } = request.params();
+    const user = await User.findOrFail(id)
+    await user.delete()
+    return response.status(202).send(user)
   }
 }
