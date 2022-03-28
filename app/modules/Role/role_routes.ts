3a573b5581837b2cf577605459f40d7124cf6463
gutkedu <eduardo.pedogutkoski@gmail.com>
2022-03-28 17:40:21 -0300

    feat/role controller & deleting bets controlelr

--- app/modules/Role/role_routes.ts
+++ app/modules/Role/role_routes.ts
@@ -1,6 +1,6 @@
 import Route from '@ioc:Adonis/Core/Route'
 
 Route.group(() => {
-  Route.resource('/bet', 'BetsController')
+  Route.resource('/roles', 'RolesController')
     .apiOnly()
 })
--- start/routes.ts
+++ start/routes.ts
@@ -1,7 +1,7 @@
 import Route from '@ioc:Adonis/Core/Route'
 import 'App/modules/User/user_routes'
 import 'App/modules/Game/game_routes'
-import 'App/modules/Bet/bet_routes'
+import 'App/modules/Role/role_routes'
 
 Route.get('/', async () => {
   return { hello: 'World from Docker' }
